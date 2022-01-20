9224e5c4c19ef36f2f5218fb6e0ba485d5a3c94f
Shaurya Sen <shauryasen12@gmail.com>
2022-01-19 21:01:01 -0500

    E

---
+++ src/main/java/frc/robot/commands/DrivetrainDrive.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+
+import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class DrivetrainDrive extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Drivetrain drivetrain;
+  private final AutoGamepad driver;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+    public DrivetrainDrive(Drivetrain subsystem, AutoGamepad gamepad) {
+
+        driver = gamepad;
+        drivetrain = subsystem;
+
+        addRequirements(subsystem);
+    }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      drivetrain.arcadeDrive(driver.getRightY() , driver.getLeftX());
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+
+  Spark leftMotor;
+  Spark rightMotor; 
+  
+
+  public Drivetrain() {
+    leftMotor = new Spark(0);
+    rightMotor = new Spark(1);
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    leftMotor.set(leftSpeed);
+    rightMotor.set(rightSpeed);
+  }
+
+  public void arcadeDrive(double speed, double rotation) {
+    tankDrive(speed + rotation, speed - rotation);
+  }
+
+  public void stop() {
+    leftMotor.set(0);
+    rightMotor.set(0);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
